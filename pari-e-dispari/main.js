f4c132ac7db3d9be15f36a495ff66363d0d82c4f
DanielePilati <danielepdj@gmail.com>
2024-05-18 15:22:45 +0200

    [upd] add prompt and functions

--- pari-e-dispari/main.js
+++ pari-e-dispari/main.js
@@ -1,16 +1,16 @@
+"use strict";
+
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-
-//chiedo in inputo un numero da 1 a 5
-//e di scegliere tra pari e dispari
-
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-
-//creo una funzione che mi genera un numero random compreso tra 1 e 5
-
-// Sommiamo i due numeri
+const userNum = Number(prompt("inserisci un numero tra 1 e 5")); //chiedo in inputo un numero da 1 a 5
+const userChoice = prompt('scegli "pari" o "dispari" ').toLowerCase; // e di scegliere tra pari e dispari
+let sum = userNum + getRandomArbitrary(1, 5); // Sommiamo i due numeri
+// Dichiariamo chi ha vinto.
 
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-
-//creo una funzione che verifica se un numero è pari o dispari
-
-// Dichiariamo chi ha vinto.
+function evenOdd(num) {
+  return num % 2 === 0;
+}
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
