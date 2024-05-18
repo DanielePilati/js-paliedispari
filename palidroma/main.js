a3f25745759d17bddcd90e3205c6cf3ec62bbd67
DanielePilati <danielepdj@gmail.com>
2024-05-18 14:37:55 +0200

    [upd] add index and main

---
+++ palidroma/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//Chiedere all’utente di inserire una parola
+
+const userWord = prompt("scrivi una parola");
+
+if (isPalindrom(userWord)) {
+  alert("è una frase palindroma");
+} else {
+  alert("non è una frase palindroma");
+}
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+function isPalindrom(word) {
+  debugger;
+  word = word.toLowerCase();
+  const characters = word.split("");
+  const reverseWord = [...characters];
+  reverseWord.reverse();
+  return characters.join("") === reverseWord.join("");
+}
